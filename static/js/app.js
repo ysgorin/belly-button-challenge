d6af6f0e3b27b8f7ac153a1c64659783f93399f6
ysgorin <ysgorin@gmail.com>
2023-04-16 21:34:33 -0400

    wrote code to update horizontal bar chart based on dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -85,6 +85,45 @@ d3.json(samplesURL).then(function(data) {
 
   // Add each case's ID to the dropdown menu
   data.names.forEach(name => dropdown.append("option").text(name).property("value", name));
+  
+  // Add event listener for drop down menu change
+  dropdown.on("change", function() {
+    // Assign displayed dropdown value to variable
+    let sample = dropdown.property("value");
+    
+    // Create a filter function
+    function filterByID(data) {
+      return data.id === sample;
+    };
+    
+    // Reassign sampleData to chosen ID
+    let sampleData = data.samples.filter(filterByID)[0];
+
+    // Update the data variables
+    let topTenIDs = sampleData.otu_ids.slice(0,10).reverse();
+    let topTenLabels = sampleData.otu_labels.slice(0,10).reverse();
+    let topTenValues = sampleData.sample_values.slice(0,10).reverse();
+
+    // Restyle horizontal bar chart with new data
+    Plotly.restyle("bar", "x", [topTenValues]);
+    Plotly.restyle("bar", "y", [topTenIDs.map(function(id) {
+          return "OTU " + id;
+        }),]);
+    Plotly.restyle("bar", "text", [topTenLabels]);
+
+    // Update demographic information
+  });
+
+    // Populate demographic information
+    // // Create a filter function with parseInt()
+    // function filterInteger(data) {
+    //   return data.id === parseInt(sample);
+    // };
+    // // Apply filter to get sample data for demographic
+    // let demoData = data.metadata.filter(filterInteger)[0];
+    // // Select demographic section using d3
+    // let demoSection = d3.select("#sample-metadata");
+    // Object.entries(demoData).forEach(([key, value]) => demoSection.append("li").text(`${key}: ${value}`));
 
 // Close the .then function
 });
